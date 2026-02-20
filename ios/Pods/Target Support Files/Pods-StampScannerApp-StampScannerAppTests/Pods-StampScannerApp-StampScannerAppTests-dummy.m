b293c5bdd3abac6ff5fe945b6e0d98ac896b75db
nithin@zinfog.com <nithinvadakkethil18@gmail.com>
2026-02-20 17:27:33 +0530

    3rd

---
+++ ios/Pods/Target Support Files/Pods-StampScannerApp-StampScannerAppTests/Pods-StampScannerApp-StampScannerAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StampScannerApp_StampScannerAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_StampScannerApp_StampScannerAppTests
+@end
